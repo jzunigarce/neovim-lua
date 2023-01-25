faa978dc5e291ad21038303ccf7545dc63696089
Jesus Antonio Zuñiga Arce <jzuniga@uabcs.mx>
2023-01-25 02:18:16 -0700

    Add lsp-zero Server

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -31,14 +31,37 @@ packer.startup(function(use)
   use {"akinsho/toggleterm.nvim"}
   use 'tpope/vim-fugitive'
   use 'editorconfig/editorconfig-vim'
+
    -- use {
     --  'nvim-treesitter/nvim-treesitter',
     -- },
    -- use {
     --  'numToStr/Comment.nvim'
     --},
-  
+  -- LSP server
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- Soporte LSP
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletado
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
 end)
+
 require("plugins/nvim-web-devicons")
 require("plugins/lualine")
 require("plugins/nvim-tree")
@@ -47,5 +70,7 @@ require("plugins/bufferline")
 require("plugins/indent-blankline")
 require("plugins/editorconfig")
 require("plugins/toogleterm-nvim")
+require("plugins/lsp-zero-nvim")
+
 -- require("plugins/treesitter-nvim")
 -- require("plugins/comment")
