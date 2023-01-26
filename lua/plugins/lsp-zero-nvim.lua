11cfa45403171133c714b465a584ed5726735671
Jesus Antonio Zuñiga Arce <jzunigarce@gmail.com>
2023-01-26 14:58:52 -0700

    Configure LSP

--- lua/plugins/lsp-zero-nvim.lua
+++ lua/plugins/lsp-zero-nvim.lua
@@ -3,4 +3,15 @@ lsp.preset('recommended')
 
 lsp.nvim_workspace()
 
+lsp.configure('tsserver', {
+  on_attach = function(client, bufnr)
+    print('Tsserver')
+  end,
+  settings = {
+    completions = {
+      completeFunctionCalls = true
+    }
+  }
+})
+
 lsp.setup()
