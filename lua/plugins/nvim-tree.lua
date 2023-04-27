96adebbfa08d087d24e5d250a6efb93e14785cf7
Jesus Antonio Zuñiga Arce <jzuniga@uabcs.mx>
2023-04-27 00:03:35 -0700

    Update key maps

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,22 +7,47 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
-local api = vim.api
+-- local api = vim.api
+
+local api = require("nvim-tree.api")
+
+local function on_attach(bufnr)
+  
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  
+  vim.keymap.set('n', 'u', api.node.navigate.parent, opts('Parent Directory'))
+  vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', 'i', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  vim.keymap.set('n', 'r', api.tree.reload, opts('Refresh'))
+  vim.keymap.set('n', 't', api.node.open.tab, opts('Open: New Tab'))
+  vim.keymap.set('n', 'R', api.fs.rename, opts('Rename'))
+  vim.keymap.set('n', 'A', api.tree.expand_all, opts('Expand All'))
+  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
+  vim.keymap.set('n', 'D', api.fs.trash, opts('Trash'))
+  vim.keymap.set('n', 'H', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
+  vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
+end
 
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-        { key = "s", action = "vsplit"},
-        { key = "i", action = "split"},
-        { key = "t", action = "tabnew"},
-        { key = "r", action = "refresh"},
-        { key = "R", action = "rename"},
-      },
-    },
+    --mappings = {
+    --  list = {
+    --    { key = "u", action = "dir_up" },
+    --   { key = "s", action = "vsplit"},
+    --    { key = "i", action = "split"},
+    --    { key = "t", action = "tabnew"},
+    --    { key = "r", action = "refresh"},
+    --    { key = "R", action = "rename"},
+    -- },
+    --},
   },
   renderer = {
     group_empty = true,
@@ -40,11 +65,11 @@ require("nvim-tree").setup({
       },
     },
   },
+  on_attach = on_attach,
 })
 
 local function open_nvim_tree()
-  local api = require("nvim-tree.api")
   api.tree.open()
 end
 
-api.nvim_create_autocmd({ "VimEnter" }, { callback = function() open_nvim_tree() end })
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = function() open_nvim_tree() end })
