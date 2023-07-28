4394c51bb9d435117751108da86a5c872eaadc87
Jesus Antonio Zuñiga Arce <jzunigarce@gmail.com>
2023-07-28 14:27:15 -0700

    Add lazygit

--- lua/plugins/toogleterm-nvim.lua
+++ lua/plugins/toogleterm-nvim.lua
@@ -2,3 +2,18 @@ require('toggleterm').setup({
   direction = 'horizontal',
   shade_terminals = true
 })
+
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ 
+  cmd = "lazygit",
+  hidden = true,
+  direction = 'float',
+  float_opts = {
+    border = 'double'
+  }
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
